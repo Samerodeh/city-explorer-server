1d530e8497a2698dcde89c1c4635d7ac97fe6dff
Samer Odeh <samerodeh220@gmail.com>
2021-06-14 20:40:05 +0300

    edit files

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
  
+const cors = require('cors');
+
+app.use(cors()) // after you initialize your express app instance
 // a server endpoint 
+
+require('dotenv').config();
+
+const port = process.env.port
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
   res.send('Hello World') // our endpoint function response
 })
  
-app.listen(3000) // kick start the express server to work 
-
-
-const cors = require('cors');
-
-app.use(cors()) // after you initialize your express app instance 
-const PORT = process.env.PORT
-
-require('dotenv').config();
+app.listen(port) // kick start the express server to work
