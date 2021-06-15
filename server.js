db72f8968d7baa5a18287eb99f6e9d9b141429e7
Samer Odeh <samerodeh220@gmail.com>
2021-06-15 14:47:57 +0300

    try solve the lab

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
+require('dotenv').config();
+ 
 const express = require('express') 
 
 const app = express() 
  
 const cors = require('cors');
 
-const data = require('./assets/weather.json');
+const weatherData = require('./assets/weather.json');
 
 const { response } = require('express');
 
 app.use(cors()) 
 
-require('dotenv').config();
 
-const port = process.env.port
+const PORT = process.env.PORT
 
 app.get('/', 
  function (req, res) { 
   res.send('Hello World') 
 }) 
 
-app.get('/weather-data', (req, res)=>{
-    res.json(data);
+app.get('/weather', (req, res)=>{ 
+  const resData = weatherData.data.map (obj => new weather (obj)); 
+    res.json(weatherData);
 });
 
+class weather { 
+
+  constructor (weatherData) {
+    this.description = weatherData.weather.description;
+    this.data = weatherData.valid_date;
+
+  }
+
+}
  
-app.listen(port, () => {
-    console.log(`Server started on ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
   });
