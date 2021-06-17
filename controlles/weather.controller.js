fec704d4066803d1a55e108fbd5db505478e9871
Samer Odeh <samerodeh220@gmail.com>
2021-06-17 09:49:22 +0300

    edit lab 09

--- controlles/weather.controller.js
+++ controlles/weather.controller.js
@@ -4,13 +4,13 @@ const Weather = require('../models/Weather.models');
 
 require('dotenv').config();
 
-const weatherData = require('./assets/weather.json');
+/* const weatherData = require('./assets/weather.json'); */
 
 const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 
 const weatherController = (req, res) => {
   
-  const resData = weatherData.data.map(obj => new weather(obj));
+  /* const resData = weatherData.data.map(obj => new weather(obj)); */
 
     const lat = req.query.lat;
     const lon = req.query.lon;
@@ -19,7 +19,7 @@ const weatherController = (req, res) => {
       const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
   
       axios.get(weatherBitUrl).then(response => {
-        const responseData = response.weatherData.data.map(obj => new Weather(obj));
+        const responseData = response.data.data.map(obj => new Weather(obj));
         res.json(responseData)
       }).catch(error => {
         res.send(error.message)
@@ -27,7 +27,7 @@ const weatherController = (req, res) => {
     } else {
       res.send('please provide the proper lat and lon')
     }
-   res.status(200).send(resData);
+  /*  res.status(200).send(resData); */
   }; 
 
 
